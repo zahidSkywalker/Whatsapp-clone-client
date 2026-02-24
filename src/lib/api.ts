361c54371c3076dffd5e5a613d186ba285fee52c
Jahidul Islam <ionzahid0987@gmail.com>
2026-02-24 11:21:40 +0600

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: `${import.meta.env.VITE_SERVER_URL}/api`,
   headers: {
     'Content-Type': 'application/json',
   },
