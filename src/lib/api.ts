0d9a6650bb84857b8608b3da12b12d287e3f320f
Jahidul Islam <ionzahid0987@gmail.com>
2026-02-24 12:48:46 +0600

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_SERVER_URL}/api`,
+  baseURL: `https://what-sapp-clone-server.onrender.com/api`,
   headers: {
     'Content-Type': 'application/json',
   },
